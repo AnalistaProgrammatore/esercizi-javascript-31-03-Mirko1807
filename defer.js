8d081f173ac654059194052b6e8d521eb45503dd
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-04-01 21:56:22 +0200

    Update defer.js

--- defer.js
+++ defer.js
@@ -1,1 +1,14 @@
+Function.prototype.defer= function(ms){
+    setTimeout(this,ms)
+}
+function hello(){
+     console.log("hello after one second")
+    }
+    
+hello.defer(1000)
 
+function cazzabubbolo(){
+    console.log ("i'm cazzabubbolo after 8 seconds")
+}
+cazzabubbolo.defer(8000)
+console.log(Function.prototype)
